744516ba4d3611d106a9384463df4e0b9c397899
yashth614 <85345922+yashth614@users.noreply.github.com>
2021-06-08 15:39:15 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,7 @@
+function Circlle(el){
+    $(el).circleProgress({fill: {color: '#ff5c5c'}})
+      .on('circle-animation-progress', function(event, progress, stepValue){
+   $(this).find('strong').text(String(stepValue.toFixed(2)).substr(2)+'%');
+   });  
+  };
+  Circlle('.round');
